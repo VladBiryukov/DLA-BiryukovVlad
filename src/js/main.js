5d32362acd5c4fd9d7407ffd71dec6c8bcbf79f9
Vlad <ksand.1@ya.ru>
2019-07-16 12:28:34 +0400

    написал блог с боиграфией о докторе Osvaldo , адаптировал

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
-var q = 'Hello world';
-console.log(q);
+var text_mobile = document.getElementsByClassName('biography-dr-Osvaldo__text_mobile')[0];
+var button_mobile = document.getElementsByClassName('biography-dr-Osvaldo__button-mobile')[0];
+var text_desktop = document.getElementsByClassName('biography-dr-Osvaldo__text_desktop')[0];
+
+ // топорный вариант, пока так
+
+button_mobile.onclick = function () {
+    text_mobile.style.display = "none";
+    text_desktop.style.display = "inline-block";
+    button_mobile.style.display="none";
+}
+
+
+ 
+
