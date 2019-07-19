3e8a49475cde7cb3d0cd61201283b0010ff3af99
Vlad <ksand.1@ya.ru>
2019-07-19 19:00:34 +0400

    добавил карту, точнее изображение, так как google maps установить не получилось

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gulp-start-pattern
 Скоро здесь будет описана документация по работе с данным gulp шаблоном
+AIzaSyDx8_lLnFyO0OV_5GTg3bUKY3BrxcQlo0U 
+<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDx8_lLnFyO0OV_5GTg3bUKY3BrxcQlo0U &callback=initMap" > -->
+
--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,15 @@ var text_mobile = document.getElementsByClassName('biography-dr-Osvaldo__text_mo
 var button_mobile = document.getElementsByClassName('biography-dr-Osvaldo__button-mobile')[0];
 var text_desktop = document.getElementsByClassName('biography-dr-Osvaldo__text_desktop')[0];
 
- // топорный вариант, пока так
+// топорный вариант, пока так
 
 button_mobile.onclick = function () {
     text_mobile.style.display = "none";
     text_desktop.style.display = "inline-block";
-    button_mobile.style.display="none";
+    button_mobile.style.display = "none";
 }
 
-
  
+
+
 
