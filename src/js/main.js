aede1df8b1d65ac6ad1346a89711915868a43d3c
Vlad <ksand.1@ya.ru>
2019-07-21 14:03:41 +0400

    перебрал позвоночник

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ var text_mobile = document.getElementsByClassName('biography-dr-Osvaldo__text_mo
 var button_mobile = document.getElementsByClassName('biography-dr-Osvaldo__button-mobile')[0];
 var text_desktop = document.getElementsByClassName('biography-dr-Osvaldo__text_desktop')[0];
 
-// топорный вариант, пока так
+ 
 
 button_mobile.onclick = function () {
     text_mobile.style.display = "none";
